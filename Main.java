ad02ff9b28fd5334ba79fb0bb4ece322fa2ecc85
Shubhankar Gupta <96997071+Shubhankargup14@users.noreply.github.com>
2023-01-19 02:50:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int currmax=0,currmin=0,sum=0;
+        for(int n:nums){
+            sum+=n;
+            currmax=Math.max(currmax+n,n);
+            currmin=Math.min(currmin+n,n);
+            max=Math.max(max,currmax);
+            min=Math.min(min,currmin);
+
+        }
+    return max<0?max:Math.max(max,sum-min);
+
+    }
 }
